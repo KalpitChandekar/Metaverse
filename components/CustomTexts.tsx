dce43a7df242a4852f9fecb1dbe0f3740f1d2add
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-10 12:39:55 +0530

    create custom title text for explore section

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -23,13 +23,13 @@ export const TypingText = ({ title, textStyles }: props) => {
   );
 };
 
-// export const TitleText = ({ title, textStyles }: any) => {
-//   <motion.h2
-//     variants={textVariant2}
-//     initial="hidden"
-//     whileInView="show"
-//     className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
-//   >
-//     {title}
-//   </motion.h2>;
-// };
+export const TitleText = ({ title, textStyles }: any) => {
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>;
+};
