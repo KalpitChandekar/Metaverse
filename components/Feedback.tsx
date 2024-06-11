cccf3cc9a8fbd9cf25891ce660e4ac398b50c942
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-11 21:04:34 +0530

    create feedback section

---
+++ components/Feedback.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
+import Image from "next/image";
+
+const Feedback = () => {
+  return (
+    <section className={`${styles.paddings}`}>
+      <motion.div
+        variants={staggerContainer as any}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+      >
+        <motion.div
+          variants={fadeIn({
+            direction: "right",
+            type: "tween",
+            delay: 0.2,
+            duration: 1,
+          })}
+          className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+        >
+          <div className="feedback-gradient" />
+          <div>
+            <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
+              Kalpit 
+            </h4>
+            <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
+              Founder Metaverus
+            </p>
+          </div>
+
+          <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+            “With the development of today`s technology, metaverse is very
+            useful for today`s work, or can be called web 3.0. by using
+            metaverse you can use it as anything”
+          </p>
+        </motion.div>
+
+        <motion.div
+          variants={fadeIn({
+            direction: "left",
+            type: "tween",
+            delay: 0.2,
+            duration: 1,
+          })}
+          className="relative flex-1 flex justify-center items-center"
+        >
+          <Image
+            src="/planet-09.png"
+            height={2200}
+            width={4700}
+            alt="planet-09"
+            className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+          />
+
+          <motion.div
+            variants={zoomIn({ delay: 0.4, duration: 1 })}
+            className="lg:block hidden absolute -left-[10%] top-[3%]"
+          >
+            <Image
+              src="/stamp.png"
+              width={495}
+              height={497}
+              alt="stamp"
+              className="w-[155px] h-[155px] object-contain"
+            />
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+export default Feedback;
